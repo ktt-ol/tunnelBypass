8c4d82e46d21df7de32c45843d563deb3467cdcd
Sebastian Reichel <sre@ring0.de>
2013-03-26 01:53:07 +0100

    simplify IP_FOUND check

--- dnsUpdater.sh
+++ dnsUpdater.sh
@@ -7,14 +7,12 @@ DOMAINS="youtube.de www.spotify.de www.spotify.com $MEDIATHEK"
 
 for d in $DOMAINS; do 
 	echo "Looking for ip for $d"
-  IP=$(nslookup $d | tail -n+5 | grep -E 'Address [0-9]+: [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | awk '{print $3}' | head -1)
-  IP_FOUND=$(echo $IP | grep -c -E '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
-  if [ ! $IP_FOUND = "1" ]; then
-  	echo "No IP for $d found!"
-  else
-  	echo "-> $IP"
-  fi
+	IP=$(nslookup $d | tail -n+5 | grep -E 'Address [0-9]+: [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | awk '{print $3}' | head -1)
+	IP_FOUND=$(echo $IP | grep -c -E '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+
+	if [ $IP_FOUND != "1" ]; then
+		echo "No IP for $d found!"
+	else
+		echo "-> $IP"
+	fi
 done
-
-
-
