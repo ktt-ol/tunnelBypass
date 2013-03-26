4861455cbab5e0a985a35668a5d6909e06972cbf
Sebastian Reichel <sre@ring0.de>
2013-03-26 02:13:13 +0100

    move domains from script into text file

--- dnsUpdater.sh
+++ dnsUpdater.sh
@@ -2,10 +2,15 @@
 # Fetches the current ip addresses for the configured domains and adds those addresses to the iptables.
 # Author: Holger Cremer
 
-MEDIATHEK="mvideos.daserste.de www.daserste.de www.ard.de www.ardmediathek.de media.tagesschau.de www.zdf.de"
-DOMAINS="youtube.de www.spotify.de www.spotify.com $MEDIATHEK"
+IFS='
+'
 
-for d in $DOMAINS; do 
+for d in $(cat domains.txt); do
+	# skip comments
+	if echo "${d}" | grep -qE "^#" ; then
+		continue
+	fi
+
 	echo "Looking for ip for $d"
 	IP=$(nslookup $d | tail -n+5 | grep -E 'Address [0-9]+: [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | awk '{print $3}' | head -1)
 	IP_FOUND=$(echo $IP | grep -c -E '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
